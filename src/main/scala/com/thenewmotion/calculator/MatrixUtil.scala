9a90c3d69c7ca6f311fedbc823111664d3fb2e92
Kostyantyn Spitsyn <k.spitsyn@thenewmotion.com>
2014-08-22 10:28:17 +0300

    improve exponentiation of matrices

--- src/main/scala/com/thenewmotion/calculator/MatrixUtil.scala
+++ src/main/scala/com/thenewmotion/calculator/MatrixUtil.scala
@@ -23,13 +23,12 @@ private[calculator] object MatrixUtil {
       (((m11 * n11 + m12 * n21) % mod, (m11 * n12 + m12 * n22) % mod),
        ((m21 * n11 + m22 * n21) % mod, (m21 * n12 + m22 * n22) % mod))
   }
-  def mxPow(m: Mx, pow: Int, mod: Int): Mx = if (pow === 1) m else mxXmx(mxPow(m, pow - 1, mod), m, mod)
 
   private val p1 = ((0, 1), (1, 1))
   private val p2 = ((0, 1), (1, 2))
 
-  val p1s: Array[Mx] = 1.to(14).map(mxPow(p1, _, 2)).toArray
-  val p2s: Array[Mx] = 1.to(14).map(mxPow(p2, _, 3)).toArray
+  val p1s: Array[Mx] = Stream.iterate(p1)(mxXmx(_, p1, 2)).take(14).toArray
+  val p2s: Array[Mx] = Stream.iterate(p2)(mxXmx(_, p2, 3)).take(14).toArray
 
   val negP2minus15 = ((0, 2), (2, 1)) // -p2^(-15)
 }
