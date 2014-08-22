963c48590842637805207620f61e1e670a323dcf
Kostyantyn Spitsyn <k.spitsyn@thenewmotion.com>
2014-08-22 11:16:11 +0300

    improve mod calculations

--- src/main/scala/com/thenewmotion/calculator/CheckDigitCalculator.scala
+++ src/main/scala/com/thenewmotion/calculator/CheckDigitCalculator.scala
@@ -7,17 +7,22 @@ object CheckDigitCalculator {
 
   def calculateCheckDigit(code: String): Char = {
 
-    def sumEq(ps: Array[Mx], part: Mx => Vec, mod: Int, code: String): Vec = {
+    def sumEq(ps: Array[Mx], part: Mx => Vec, code: String): Vec = {
       val vecs = 0.to(13).map{ x =>
         val qr: Vec = part(encoding.getOrElse(code.charAt(x), sys.error("Unencodable character")))
         val p: Mx = ps(x)
-        vecXMx(qr, p, mod)
+        vecXMx(qr, p)
       }
-      vecs.foldLeft((0, 0))(sumVec(_, _, mod))
+      vecs.foldLeft((0, 0))(sumVec)
     }
 
-    val t1 = sumEq(p1s, _._1, 2, code)
-    val t2 = vecXMx(sumEq(p2s, _._2, 3, code), negP2minus15, 3)
+    val t1 = sumEq(p1s, _._1, code) match {
+      case (q1 ,q2) => (q1 % 2, q2 % 2)
+    }
+
+    val t2 = vecXMx(sumEq(p2s, _._2, code), negP2minus15) match {
+      case (r1, r2) => (r1 % 3, r2 % 3)
+    }
 
     decoding.getOrElse((t1, t2), sys.error("Undecodable matrix"))
   }
--- src/main/scala/com/thenewmotion/calculator/MatrixUtil.scala
+++ src/main/scala/com/thenewmotion/calculator/MatrixUtil.scala
@@ -8,20 +8,20 @@ private[calculator] object MatrixUtil {
   type Vec = (Int, Int)
   type Mx = (Vec, Vec)
 
-  def vecXMx(vec: Vec, mx: Mx, mod: Int): Vec = (vec, mx) match {
+  def vecXMx(vec: Vec, mx: Mx): Vec = (vec, mx) match {
     case ((v1, v2), ((m11, m12), (m21, m22))) =>
-      ((v1 * m11 + v2 * m21) % mod,
-       (v1 * m12 + v2 * m22) % mod)
+      (v1 * m11 + v2 * m21,
+       v1 * m12 + v2 * m22)
   }
 
-  def sumVec(v: Vec, w: Vec, mod: Int): Vec = (v, w) match {
-    case ((v1, v2), (w1, w2)) => ((v1 + w1) % mod, (v2 + w2) % mod)
+  def sumVec(v: Vec, w: Vec): Vec = (v, w) match {
+    case ((v1, v2), (w1, w2)) => (v1 + w1, v2 + w2)
   }
 
   def mxXmx(m: Mx, n: Mx, mod: Int): Mx = (m, n) match {
     case (((m11, m12), (m21, m22)), ((n11, n12), (n21, n22))) =>
-      (((m11 * n11 + m12 * n21) % mod, (m11 * n12 + m12 * n22) % mod),
-       ((m21 * n11 + m22 * n21) % mod, (m21 * n12 + m22 * n22) % mod))
+      ((m11 * n11 + m12 * n21, m11 * n12 + m12 * n22),
+       (m21 * n11 + m22 * n21, m21 * n12 + m22 * n22))
   }
 
   private val p1 = ((0, 1), (1, 1))
