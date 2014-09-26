0d78aa929057f1d14821f68ba7ca529d8e48652b
Reinier Lamers <r.lamers@thenewmotion.com>
2014-09-26 17:06:45 +0200

    Give this library another end-user interface

---
+++ src/main/scala/com/thenewmotion/evcoid/CheckDigit.scala
@@ -0,0 +1,76 @@
+package com.thenewmotion.evcoid
+
+private[evcoid] object CheckDigit {
+
+  import MatrixUtil._
+  import LookupTables._
+
+  def apply(code: String): Char = {
+    require(code.size == p1s.size && code.size == p2s.size && code.forall(_.isLetterOrDigit))
+
+    @annotation.tailrec
+    def sumEq(ps: Array[Matrix], f: Matrix => Vec, v: Vec = Vec(0, 0), i: Int = 0): Vec = {
+      if (i < ps.length) {
+        val ch = code.charAt(i)
+        val qr = f(encoding.getOrElse(ch, sys.error(s"Invalid character: $ch.")))
+        sumEq(ps, f, v + (qr * ps(i)), i + 1)
+      } else v
+    }
+
+    val t1 = sumEq(p1s, m => Vec(m.m11, m.m12))
+    val t2 = sumEq(p2s, m => Vec(m.m21, m.m22)) * negP2minus15
+    val m15 = Matrix(t1.v1 & 1, t1.v2 & 1, t2.v1 % 3, t2.v2 % 3)
+    decoding.getOrElse(m15, sys.error(s"Undecodable matrix: $m15."))
+  }
+
+}
+
+private[evcoid] object LookupTables {
+  import MatrixUtil.Matrix
+
+  def decode(x: Int): Matrix = Matrix(x & 1, (x >> 1) & 1, (x >> 2) & 3, x >> 4)
+
+  def encode(mx: Matrix): Int = mx.m11 + (mx.m12 << 1) + (mx.m21 << 2) + (mx.m22 << 4)
+
+  val encoding: Map[Char, Matrix] = Map[Char, Int](
+    '0' -> 0, '1' -> 16, '2' -> 32,
+    '3' -> 4, '4' -> 20, '5' -> 36,
+    '6' -> 8, '7' -> 24, '8' -> 40,
+    '9' -> 2, 'A' -> 18, 'B' -> 34,
+    'C' -> 6, 'D' -> 22, 'E' -> 38,
+    'F' -> 10, 'G' -> 26, 'H' -> 42,
+    'I' -> 1, 'J' -> 17, 'K' -> 33,
+    'L' -> 5, 'M' -> 21, 'N' -> 37,
+    'O' -> 9, 'P' -> 25, 'Q' -> 41,
+    'R' -> 3, 'S' -> 19, 'T' -> 35,
+    'U' -> 7, 'V' -> 23, 'W' -> 39,
+    'X' -> 11, 'Y' -> 27, 'Z' -> 43
+  ).mapValues(decode)
+
+  val decoding: Map[Matrix, Char] = encoding.map(_.swap)
+}
+
+private[evcoid] object MatrixUtil {
+
+  case class Matrix(m11: Int, m12: Int, m21: Int, m22: Int) {
+    def *(m: Matrix): Matrix =
+      Matrix(m11 * m.m11 + m12 * m.m21, m11 * m.m12 + m12 * m.m22,
+        m21 * m.m11 + m22 * m.m21, m21 * m.m12 + m22 * m.m22)
+  }
+
+  case class Vec(v1: Int, v2: Int) {
+    def +(v: Vec): Vec = Vec(v1 + v.v1, v2 + v.v2)
+
+    def *(m: Matrix): Vec = new Vec(v1 * m.m11 + v2 * m.m21, v1 * m.m12 + v2 * m.m22)
+  }
+  private val p1 = Matrix(0, 1, 1, 1)
+  private val p2 = Matrix(0, 1, 1, 2)
+
+  val p1s: Array[Matrix] = Stream.iterate(p1)(_ * p1).take(14).toArray
+  val p2s: Array[Matrix] = Stream.iterate(p2)(_ * p2).take(14).toArray
+
+  val negP2minus15 = Matrix(0, 2, 2, 1) // -p2^(-15)
+}
+
+
+
---
+++ src/test/scala/com/thenewmotion/evcoid/EmaIdSpec.scala
@@ -0,0 +1,66 @@
+package com.thenewmotion.evcoid
+
+import org.specs2.mutable.SpecificationWithJUnit
+
+class EmaIdSpec extends SpecificationWithJUnit {
+
+  "EmaId" should {
+    "render an EMAID in the normalized form with dashes and check digit" in {
+      EmaId("NL", "TNM", "000722345").toString mustEqual "NL-TNM-000722345-X"
+    }
+
+    "refuse input fields with wrong lengths" in {
+      EmaId("A", "TNM", "000122045") must throwA[IllegalArgumentException]
+      EmaId("NL", "TNMN", "000722345") must throwA[IllegalArgumentException]
+      EmaId("NL", "TNM", "72245") must throwA[IllegalArgumentException]
+      EmaId("NL", "T|M", "000122045") must throwA[IllegalArgumentException]
+    }
+
+    "create an EmaId from a string in any DIN SPEC 91286 or ISO 15118 format" in {
+      List(EmaId("NL-TNM-000122045-U"),
+        EmaId("NL-TNM-000122045-U"),
+        EmaId("NLTNM000122045"),
+        /*           EmaId("NL-TNM-012204"), */ // TODO check digit computation for DIN IDs?
+        EmaId("NL-TNM-012204-5")) mustEqual List.fill(4)(Some(EmaId("NL", "TNM", "000122045")))
+    }
+
+    "return None when trying to create an EmaId from an invalid string" in {
+      List(EmaId("NL-TNM-000122045-X"), // wrong check digit
+           EmaId("NLTNM076"), // wrong length
+           EmaId("X-aargh-131331234"), // wrong length of fields
+           EmaId(" \0t24\u2396a\t"), // whatever nonsense
+           EmaId("NL-T|M-000122045-U") // right lengths but illegal char
+      ) mustEqual List.fill(5)(None)
+    }
+
+    "render an EMAID as a compact string without dashes" in {
+      EmaId("NL", "TNM", "000722345").toCompactString mustEqual "NLTNM000722345X"
+    }
+
+    "render an EMAID as a compact string without dashes and without check digit" in {
+      EmaId("NL", "TNM", "000722345").toCompactStringWithoutCheckDigit mustEqual "NLTNM000722345"
+    }
+
+    "render an EMAID as a DIN SPEC 91286 string if applicable" in {
+      EmaId("NL", "TNM", "000122045").toDinString mustEqual Some("NL-TNM-012204-5")
+    }
+
+    "return None when trying to render a non-DIN EMAID as a DIN string" in {
+      EmaId("NL", "TNM", "012345678").toDinString must beNone
+    }
+
+    "be unapplicable" in {
+      EmaId("NL", "TNM","000122045") match {
+        case EmaId(c, p, i, ch) => (c, p, i, ch) mustEqual ("NL", "TNM", "000122045", 'U')
+      }
+    }
+
+    "be equal to an object representing the same ID created with different arguemnts" in {
+      Some(EmaId("NL", "TNM", "000122045")) mustEqual EmaId("NL-TNM-012204-5")
+
+      EmaId("NL-TNM-000122045") mustEqual Some(EmaId("NL", "TNM", "000122045", 'U'))
+
+      EmaId("NLTNM012345678") mustNotEqual EmaId("NLTNM876543210")
+    }
+  }
+}
