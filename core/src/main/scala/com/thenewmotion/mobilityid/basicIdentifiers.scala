f4c3081ed5eab41f94f8bac5c964e5ee33301d78
Christoph Zwirello <c.zwirello@newmotion.com>
2021-01-14 09:49:40 +0100

    don't bother with backwards-compatibility

--- core/src/main/scala/com/thenewmotion/mobilityid/basicIdentifiers.scala
+++ core/src/main/scala/com/thenewmotion/mobilityid/basicIdentifiers.scala
@@ -30,11 +30,10 @@ private case class CountryCodeImpl(cc: String) extends CountryCode {
 object CountryCode {
   val Regex = """([A-Za-z]{2})""".r
 
-  lazy val isoCountries: Array[String] = Locale.getISOCountries
-  private val isoCountriesSet = isoCountries.toSet
+  lazy val isoCountries: Set[String] = Locale.getISOCountries.toSet
 
   def isValid(countryCode: String): Boolean = countryCode match {
-    case Regex(_) if isoCountriesSet.contains(countryCode.toUpperCase) => true
+    case Regex(_) if isoCountries.contains(countryCode.toUpperCase) => true
     case _ => false
   }
 
